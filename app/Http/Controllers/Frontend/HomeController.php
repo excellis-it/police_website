6f5bfc3030fba066185f0488c838c7321062695a
excellis-it <web.excellisit@gmail.com>
2024-07-08 19:16:22 +0530

    add

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -12,7 +12,8 @@ class HomeController extends Controller
     public function index()
     {
         // get the user whose created at date is today to 48 hours
-        $users = User::role('CRIMINAL')->where('status', 0)->whereBetween('created_at', [now()->subDays(2), now()])->get();
+        // $users = User::role('CRIMINAL')->where('status', 0)->whereBetween('created_at', [now()->subDays(2), now()])->get();
+        $users = User::role('CRIMINAL')->where('status', 0)->get();
         $slider_times = SliderTime::all();
         return view('frontend.home')->with(compact('users', 'slider_times'));
     }
